d763b4138ca08c80174a513cd8c8f7ba50b68667
tteo6078 <tzeyingteoh23@gmail.com>
2025-11-20 12:13:07 +1100

    mobile active share

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const shareBtn = document.getElementById("share-btn");
+const closeBtn = document.getElementById("close-share");
 const profile = document.querySelector(".profile");
+const sharePopup = document.querySelector(".share-popup");
 
 shareBtn.addEventListener("click", () => {
-profile.classList.toggle("active");
+  profile.classList.add("hidden");
+  sharePopup.classList.add("is-open");
+  sharePopup.classList.remove("hidden");
+});
+
+closeBtn.addEventListener("click", () => {
+  sharePopup.classList.remove("is-open");
+  sharePopup.classList.add("hidden");
+  profile.classList.remove("hidden");
 });
